a8b1bdda0a03d998eeb026a1143d222abb8408dc
feupHelder <32600751+feupHelder@users.noreply.github.com>
2019-08-07 12:08:27 +0100

    Add files via upload

--- AP/AP.ino
+++ AP/AP.ino
@@ -3,6 +3,7 @@
 #include <WebServer.h>
 #include <WiFiAP.h>
 #include "index.h"
+#include <SPI.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,28 +20,75 @@ uint8_t temprature_sens_read();
 WebServer server(80);
 
 
+static const int spiClk = 40000000; // 1 MHz
 
+//uninitalised pointers to SPI objects
+SPIClass * vspi = NULL;
 
 // Set these to your desired credentials.
 const char *ssid = "teste";
 const char *password = "1234567890";
 
+hw_timer_t * timerStim = NULL;
+hw_timer_t * timerCharge = NULL;
 hw_timer_t * timer = NULL;
+hw_timer_t * timerRead_ = NULL;
+
 volatile SemaphoreHandle_t timerSemaphore;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 volatile uint32_t isrCounter = 0;
 volatile uint32_t lastIsrAt = 0;
+
+volatile uint32_t CicleTimeCounter = 0;
+volatile uint32_t StimulusTimeCounter = 0;
+
+volatile uint32_t CicleTime = 10000;
+volatile uint32_t StimulusTime = 100;
+volatile uint32_t Amplitude = 255;
+volatile bool criticalRegion= false;
+
+volatile uint8_t  data[1000];
+uint8_t n=1000;
+
+volatile bool cicleDone = false;
+volatile bool stimFlag = false;
 volatile bool led = false;
 
-//periodic task with top priority
-void IRAM_ATTR onTimer() {
-  portENTER_CRITICAL_ISR(&timerMux);
-  digitalWrite(LED_BUILTIN, led);
-  led = !led;
-  portEXIT_CRITICAL_ISR(&timerMux);
-  // Give a semaphore that we can check in the loop
-  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+//periodic task - T=100us
+void IRAM_ATTR timerFunc() {
+  CicleTimeCounter++;
+
+  if (CicleTimeCounter == CicleTime ) {
+    CicleTimeCounter = 0;
+    stimFlag = true;
+  }
+  if (stimFlag) StimulusTimeCounter++;
+  if (StimulusTimeCounter == StimulusTime ) {
+    StimulusTimeCounter = 0;
+    stimFlag = false;
+  }
+}
+
+void IRAM_ATTR Stim() {
+  if (stimFlag) {
+    digitalWrite(2, HIGH);
+  }
+  else {
+    digitalWrite(2, LOW);
+  }
+}
+//Parar de estimular quando se muda a tensão de estimulo?
+void IRAM_ATTR charge() {
+  SPItoDAC( 1, Amplitude, 1);
+}
+
+void IRAM_ATTR read() {
+  for(int i=0;i<n-1;i++)
+    {
+        data[i]=data[i+1];
+    }
+  data[n-1] = temprature_sens_read();
 }
 
 void handleRoot() {
@@ -50,41 +98,220 @@ void handleRoot() {
 
 
 void handleForm() {
-  String V = server.arg("Voltage");
+  Amplitude = map(server.arg("Voltage").toDouble(), 0, 5, 0, 255);
+  CicleTime = server.arg("Cicle Time").toDouble();
+  StimulusTime = server.arg("Stimulus Time").toDouble();
 
-  Serial.print("Voltage:");
-  Serial.println(V);
 
   String s = MAIN_page; //Read HTML contents
   server.send(200, "text/html", s); //Send web page
 }
 
 void handleSensor() {
-//  int a = analogRead(A0);
-//  String adcValue = String(a);
-  server.send(200, "text/plane", String((temprature_sens_read() - 32) / 1.8)); //Send ADC value only to client ajax request
+  //  int a = analogRead(A0);
+  //  String adcValue = String(a);
+  String send;
+  for(int i=90; i<n; i++){
+    send=send+data[i]+"; ";
+  }
+  server.send(200, "text/plane", send); //Send ADC value only to client ajax request
+}
+
+void NivelTensao (char modo_volts, char volts_ext) {
+
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE1));
+  digitalWrite(5, LOW); //pull SS slow to prep other end for transfer
+  vspi->transfer(modo_volts);
+  vspi->transfer(volts_ext);
+  digitalWrite(5, HIGH); //pull ss high to signify end of data transfer
+  vspi->endTransaction();
+}
+
+void SPItoDAC(int polaridade, int tensao, int pares) {
+  int aux = 0x0;
+  int valor = 0x0;
+
+  for (int i = 0; i < tensao; i++)
+  {
+    aux++;
+  }
+  NivelTensao (highByte(0x9000), lowByte(0x9000));
+
+  // // Serial.print(aux);
+  // Serial.print(" ");
+  if (pares == 1 && polaridade == 1) {
+    valor = ( 0 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x10 );
+    NivelTensao (highByte(valor), lowByte(valor));
+
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+
+  if (pares == 2 && polaridade == 1) {
+    valor = ( 0x1 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" "); valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x20 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+
+  if (pares == 3 && polaridade == 1) {
+    valor = ( 0x2 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x40 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+  if (pares == 4 && polaridade == 1) {
+    valor = ( 0x3 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x80 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+  if (pares == 1 && polaridade == 2) {
+    valor = ( 0x4 << 12 | aux << 4 | 0x0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = (0xF << 12 | 0 << 8 | 0x1 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+
+  if (pares == 2 && polaridade == 2) {
+    valor = ( 0x5 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x2 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+  if (pares == 3 && polaridade == 2) {
+    valor = ( 0x6 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x4 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
+  if (pares == 4 && polaridade == 2) {
+    valor = ( 0x7 << 12 | aux << 4 | 0 );
+    NivelTensao (highByte(valor), lowByte(valor));
+
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+    valor = 0x0;
+
+    valor = ( 0xF << 12 | 0 << 8 | 0x8 );
+    NivelTensao (highByte(valor), lowByte(valor));
+    // Serial.print(highByte(valor));
+    // Serial.print(" ");
+    // Serial.print(lowByte(valor));
+    // Serial.print(" ");
+  }
 }
 
 
+
 void setup() {
-  timerSemaphore = xSemaphoreCreateBinary();
+  vspi = new SPIClass(VSPI);
+  vspi->begin();
+  pinMode(5, O UTPUT); //VSPI SS
+
+  //timerSemaphore = xSemaphoreCreateBinary();
 
   // Use 1st timer of 4 (counted from zero).
   // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more
   // info).
-  timer = timerBegin(0, 80, true);
+  timerStim = timerBegin(0, 80, true);
+  timerCharge = timerBegin(1, 80, true);
+  timer = timerBegin(2, 80, true);
+  timerRead_ = timerBegin(3, 80, true);
 
   // Attach onTimer function to our timer.
-  timerAttachInterrupt(timer, &onTimer, true);
+  timerAttachInterrupt(timerStim, &Stim, true);
+  timerAttachInterrupt(timerCharge, &charge, true);
+  timerAttachInterrupt(timer, &timerFunc, true);
+  timerAttachInterrupt(timerRead_, &read, true);
 
   // Set alarm to call onTimer function every second (value in microseconds).
   // Repeat the alarm (third parameter)
-  timerAlarmWrite(timer, 500, true);
+  timerAlarmWrite(timerStim, 50, true);
+  timerAlarmWrite(timerCharge, 1000000, true); ///_________________________________
+  timerAlarmWrite(timer, 100, true);
+  timerAlarmWrite(timerRead_, 100000, true);
 
   // Start an alarm
+  timerAlarmEnable(timerStim);
+  timerAlarmEnable(timerCharge);
   timerAlarmEnable(timer);
+  timerAlarmEnable(timerRead_);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(22, OUTPUT);
 
   Serial.begin(115200);
   Serial.println();
@@ -93,12 +320,12 @@ void setup() {
   // You can remove the password parameter if you want the AP to be open.
   WiFi.softAP(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
+  // Serial.print("AP IP address: ");
   Serial.println(myIP);
 
   server.on("/", handleRoot);      //Which routine to handle at root location
   server.on("/action_page", handleForm); //form action is handled here
-  server.on("/readSensor", handleSensor);//To get update of ADC Value only
+  server.on("/readData", handleSensor);//To get update of ADC Value only
 
   server.begin();
 
--- AP/index.h
+++ AP/index.h
@@ -1,6 +1,7 @@
 const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
+<head>
 <style>
 .card{
     max-width: 400px;
@@ -10,41 +11,61 @@ const char MAIN_page[] PROGMEM = R"=====(
      box-sizing: border-box;
      color: #FFF;
      margin:20px;
+     font-size: 15px;
      box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
 }
+.table{
+      max-width: 400px;
+     min-height: 250px;
+     background: #02b875;
+     padding: 30px;
+     box-sizing: border-box;
+     color: #FFF;
+     margin:20px;
+      font-size: 15px;
+     box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+}
+
 </style>
+</head>
 <body>
 
 <div class="card">
-  <h1>Sensor Value:<span id="SensorValue">0</span></h1><br>
-  
-
   <form action="/action_page">
   Voltage:<br>
-  <input type="number" name="Voltage" max="5" min="0" step="0.01">
+  <input type="number" name="Voltage" max="255" min="0" step="0.01">
+  <br><br>
+  CicleTime:<br>
+  <input type="number" name="Cicle Time" step="0.01">
+  <br><br>
+  StimulusTime:<br>
+  <input type="number" name="Stimulus Time" step="0.01">
   <br><br>
   <input type="submit" value="Submit">
   </form> 
 
 </div>
 
+<div class="table">
+Data:<span id="Data">0</span>  <font-size= 15px;<br>
 
+</div>
 <script>
 
 setInterval(function() {
-  // Call a function repetatively with 2 Second interval
+  
   getData();
-}, 500); //2000mSeconds update rate
+}, 1000); //mSeconds update rate
 
 function getData() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("SensorValue").innerHTML =
+      document.getElementById("Data").innerHTML =
       this.responseText;
     }
   };
-  xhttp.open("GET", "readSensor", true);
+  xhttp.open("GET", "readData", true);
   xhttp.send();
 }
 </script>
